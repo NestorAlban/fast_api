e94ea63cfb1e3b595193b8fc30064c401b36c1de
Nestor Alban <albquenes7294gmail.com>
2022-02-04 01:58:11 -0500

    validations_doby

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Python
 from typing import Optional
+from unittest import result
 
 #Pydantic
 from pydantic import BaseModel
@@ -11,6 +12,11 @@ from fastapi import Body, Query, Path
 app= FastAPI()
 
 #Models
+
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
 
 class Person(BaseModel):
     first_name: str
@@ -55,3 +61,21 @@ def show_person(
     person_id: int = Path(...,gt=0)
 ):
     return {person_id: "It exists!"}
+
+
+#validaciones: request body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id:int=Path(
+        ...,
+        title="Person ID",
+        description="This is the person ID",
+        gt=0
+    ),
+    person: Person=Body(...),
+    location: Location=Body(...)
+):
+    results=person.dict()
+    results.update(location.dict())
+    return results
