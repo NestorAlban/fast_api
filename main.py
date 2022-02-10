c8b97d5495b41c4ebe972f414fa9ac90ac4d1288
Nestor Alban <albquenes7294gmail.com>
2022-02-10 08:30:42 -0500

    response_model

--- main.py
+++ main.py
@@ -47,6 +47,29 @@ class Person(BaseModel):
     )
     hair_color: Optional[HairColor]=Field(default=None, example="black")
     is_married: Optional[bool]=Field(default=None, example=False)
+    password: str=Field(..., min_length=8)
+
+class PersonOut(BaseModel):
+    first_name: str =Field(
+        ..., 
+        min_length=1,
+        max_length=50,
+        example="Miguel"
+        )
+    last_name: str=Field(
+        ..., 
+        min_length=1,
+        max_length=50,
+        example="Torres"
+        )
+    age: int=Field(
+        ...,
+        gt=0,
+        le=115,
+        example=20
+    )
+    hair_color: Optional[HairColor]=Field(default=None, example="black")
+    is_married: Optional[bool]=Field(default=None, example=False)
 
 #    class Config:
 #        schema_extra={
@@ -65,7 +88,7 @@ def home():
 
 #Request and response body
 
-@app.post("/person/new")
+@app.post("/person/new", response_model=PersonOut)
 def create_person(person: Person=Body(...)):
     return person
 
