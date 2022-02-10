8d9c8a0bd2849d91b144702d55f0f1e89b2ef6ed
Nestor Alban <albquenes7294gmail.com>
2022-02-10 09:34:31 -0500

    cookies_and_headers

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 #Python
-from email.policy import default
-from importlib.resources import path
-from tkinter.tix import Form
+from os import stat
 from typing import Optional
 from enum import Enum
+
 
 #Pydantic
 from pydantic import BaseModel
 from pydantic import Field
+from pydantic import EmailStr
 
 #FastAPI
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie
 
 app= FastAPI()
 
@@ -147,6 +147,8 @@ def update_person(
     #return results
     return person
 
+#Forms
+
 @app.post(
     path="/login",
     response_model=LoginOut,
@@ -157,3 +159,30 @@ def login(
     password: str=Form(...)
     ):
     return LoginOut(username=username)
+
+#Cookies and Headers Parameters
+
+@app.post(
+    path="/contact",
+    status_code=status.HTTP_200_OK
+)
+def contact(
+    first_name: str=Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    last_name: str=Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    email: EmailStr=Form(...),
+    message: str=Form(
+        ...,
+        min_length=20
+    ),
+    user_agent: Optional[str]=Header(default=None),
+    ads: Optional[str]=Cookie(default=None)
+):
+    return user_agent
