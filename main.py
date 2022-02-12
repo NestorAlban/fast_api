40d104d4e666ecd79c1053484ce3cdd7bdcb60f1
Nestor Alban <albquenes7294gmail.com>
2022-02-12 03:29:09 -0500

    tags

--- main.py
+++ main.py
@@ -93,7 +93,8 @@ def home():
 @app.post(
     path="/person/new", 
     response_model=PersonOut,
-    status_code=status.HTTP_201_CREATED
+    status_code=status.HTTP_201_CREATED,
+    tags=["Persons"]
     )
 def create_person(person: Person=Body(...)):
     return person
@@ -102,7 +103,8 @@ def create_person(person: Person=Body(...)):
 
 @app.get(
     path="/person/detail",
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    tags=["Persons"]
     )
 def show_person (
     name: Optional[str]=Query(
@@ -124,7 +126,10 @@ def show_person (
 
 persons=[1,2,3,4,5]
 
-@app.get("/person/detail/{person_id}")
+@app.get(
+    path= "/person/detail/{person_id}", 
+    tags=["Persons"]
+    )
 def show_person(
     person_id: int = Path(
     ...,
@@ -141,7 +146,10 @@ def show_person(
 
 #validaciones: request body
 
-@app.put("/person/{person_id}")
+@app.put(
+    path="/person/{person_id}",
+    tags=["Persons"]
+    )
 def update_person(
     person_id:int=Path(
         ...,
@@ -162,7 +170,8 @@ def update_person(
 @app.post(
     path="/login",
     response_model=LoginOut,
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    tags=["Persons"]
 )
 def login(
     username: str=Form(...), 
